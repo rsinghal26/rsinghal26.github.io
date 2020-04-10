dd058de21375710a9cf4e43355d7289c26781c38
rsinghal26 <singhalraja.30003@gmail.com>
2020-04-10 21:09:08 +0530

    update v1.0

---
+++ js/main.js
@@ -0,0 +1,78 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+	var getHeight = function() {
+		var extraHeight = 0;
+
+		if ( isMobile.any() ) extraHeight = 50;
+		
+		setTimeout(function(){
+			$('#rjp-main').stop().animate({
+				'height': $('.rjp-tab-content.active').height() + extraHeight
+			});
+		}, 200);
+	};
+
+
+	var tabContainer = function() {
+		getHeight();
+		$(window).resize(function(){
+			getHeight();
+		})
+	};
+
+	var tabClickTrigger = function() {
+		$('.rjp-tab-menu a').on('click', function(event) {
+			event.preventDefault();
+			var $this = $(this),
+				data = $this.data('tab'),
+				pie = $this.data('pie');
+
+			// add/remove active class
+			$('.rjp-tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active');
+
+			$('.rjp-tab-content.active').addClass('animated fadeOutDown');
+
+			setTimeout(function(){
+				$('.rjp-tab-content.active').removeClass('active animated fadeOutDown fadeInUp');
+				$('.rjp-tab-content[data-content="'+data+'"]').addClass('animated fadeInUp active');
+				getHeight();
+			}, 500);
+			
+		})
+	};
+
+	// Document on load.
+	$(function(){
+		tabContainer();
+		tabClickTrigger();
+
+	});
+
+
+}());
